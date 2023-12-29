4a89592e6661d3d68eadeb8d43a868cb2f826a7e
Gowthamigidda <116887857+Gowthamigidda@users.noreply.github.com>
2023-12-29 11:30:18 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ from PIL import Image
 st.set_page_config(page_title='cats')
 st.markdown("## Types of Cats")
 st.write("-Persian Cat")
-image = Image.open('C:\Users\Administrator\Downloads\perse.jpg')
+image = Image.open(r'C:\Users\Administrator\Downloads\perse.jpg')
 st.image(image, caption='Enter any caption here')
 st.write("-White Cat")
 
