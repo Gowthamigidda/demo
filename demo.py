b718feff4928c2d3bbe872f9e821d592fb5bf6a3
Gowthamigidda <116887857+Gowthamigidda@users.noreply.github.com>
2023-12-29 11:34:22 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ st.set_page_config(page_title='cats')
 st.markdown("## Types of Cats")
 st.write("-Persian Cat")
 image = Image.open('./perse.jpg')
-st.image(image, caption='Enter any caption here')
+st.image(image)
 st.write("-White Cat")
+st.image("./perse.jpg")
 
