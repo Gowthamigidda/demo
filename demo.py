a81627abfdfde05c92631ad69ba7014d2ebce6d5
Gowthamigidda <116887857+Gowthamigidda@users.noreply.github.com>
2023-12-29 12:08:11 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,10 @@
 import streamlit as st
 st.set_page_config(page_title='cats')
-st.markdown("## Types of Cats")
-st.write("-White Cat")
-st.image("https://www.rover.com/blog/wp-content/uploads/white-cat-min.jpg",width=600)
-st.write("-Persian Cat")
-st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt8T7X9bO9-L1tUZw-C-PwSSD973LYAxcYaF4dqykXW2HmOx9aMKuRlhP-0Q&s",width=600)
+st.header("Types of Cats")
+c1,c2=st.columns(2)
+with c1:
+  st.write("-White Cat")
+  st.image("https://www.rover.com/blog/wp-content/uploads/white-cat-min.jpg",width=600)
+with c2:
+  st.write("-Persian Cat")
+  st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt8T7X9bO9-L1tUZw-C-PwSSD973LYAxcYaF4dqykXW2HmOx9aMKuRlhP-0Q&s",width=600)
